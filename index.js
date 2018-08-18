1dd795038f70a5df37cad6290e95b6f8e814ee91
tatsuya kobayashi <tkey.dll@gmail.com>
2018-08-18 18:41:54 +0900

    first commit.

---
+++ index.js
@@ -0,0 +1,48 @@
+
+var fs = require('fs');
+var readline = require('readline');
+
+var path = process.argv[2];
+if (path == undefined)
+{
+    console.log("usage: node index.js your_hatena_export_data");
+    console.log("");
+    return;
+}
+
+var reader = readline.createInterface({
+    input: fs.createReadStream(path, 'utf8')
+})
+
+var doc = new Object;
+doc.pages = new Array();
+var obj;
+var isBody = false;
+
+reader.on('line', (data) => {
+    if (data.match('^(TITLE: )')) {
+        obj = new Object();
+        obj.title = data.split(': ')[1];
+        obj.lines = new Array();
+        obj.lines.push(obj.title);
+    }
+    else if (data.match('^(BODY:)')) {
+        isBody = true;
+    }
+    else if ((isBody == true) && (data == '-----')) {
+        isBody = false;
+    }
+    else if (data == '--------') {
+        doc.pages.push(obj);
+    }
+    else if (isBody == true) {
+        obj.lines.push(data);
+    }
+    //console.log(line);
+});
+
+reader.on('close', () => {
+    var json = JSON.stringify(doc);
+    console.log(json);
+});
+    
